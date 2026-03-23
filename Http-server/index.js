eefa34b49dd047b0f280c9a13119ca81943d9222
Aditya <adityasara2622@gmail.com>
2026-03-23 22:53:02 +0530

    servers

--- Http-server/index.js
+++ Http-server/index.js
@@ -1,12 +1,20 @@
-// in this file we will create a simple http server using nodejs
+// in this file we will create a simple http server using nodejs and we will log all the incoming requests to a file called server.log
 
 // first we need to import the http module
 const http = require('http');
 
+const fs = require('fs');
+
 // then we need to create a server
 const server = http.createServer((req, res) => {
-    // this function will be called every time we get an incoming request
+    const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n ${req.url}\n`;
+    fs.appendFile('server.log', log, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+        }
+    });
 
+    // this function will be called every time we get an incoming request
     console.log("i got an incomming request");
     // we can write the response to the client using the res object
     res.writeHead(200, { 'Content-Type': 'text/plain' });
